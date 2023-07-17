e36ef067a11b5f0d585ee3d1b8d1f0412342ec4e
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2023-07-17 12:35:28 -0500

    cahnges

--- routes/application.js
+++ routes/application.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 //const usersValidation = require('../middleware/usersValidation');
 
-const recruitersValidation = require('../middleware/applicationvalidator');
+
 const recruitersController = require('../controllers/application');
 const schema = require('../helper/validation_schema');
 const middleware = require('../middleware/validation_middleware');
@@ -24,11 +24,10 @@ router.get("/position/:position", recruitersController.getJobsByPosition, (req, 
     
 });
 
-router.post('/', recruitersController.createRecruiter, (req, res) => {
-    res.json(req.body); 
+router.post('/', middleware(schema.validateapp), recruitersController.createRecruiter, (req, res) => {
+    res.json(req.body);
 });
 
-
-router.put('/:id', recruitersValidation.saveRecruiter, recruitersController.updateRecruiter);
-
+router.put('/:id', middleware(schema.validateapp), recruitersController.updateRecruiter, (req, res) => {
+    res.json(req.body);
 module.exports = router;
